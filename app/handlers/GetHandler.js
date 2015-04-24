7bc46f2ac94d4f477ad0ab31d1bd7070dbbdae50
yanning92 <ebelayannick@yahoo.fr>
2015-04-24 23:55:30 +0200

    get menuitems and post one menu item

--- app/handlers/GetHandler.js
+++ app/handlers/GetHandler.js
@@ -3,21 +3,35 @@
  */
 'use strict';
 var TodoService = require('../services/todoService');
+var MenuService = require('../services/menuService');
 function GetHandler(server) {
     if (!(this instanceof GetHandler)) {
         return new GetHandler(server);
     }
     GetHandler.prototype.server = server;
-};
-
+}
 GetHandler.prototype.getAll = function getAll(req, reply) {
-    var todoService = new TodoService();
+    var todoService = new MenuService();
     todoService.getTodos().then(function (data) {
         reply(data)
             .code(200)
             .header('Content-Type', 'application/json');
     }).catch(function (error) {
        // reply(boom.server_error(error.message))
+        reply('error')
+            .code(500)
+            .type('application/json');
+    });
+};
+
+GetHandler.prototype.getAllMenuItems = function getAllMenuItems(req, reply) {
+    var menuService = new MenuService();
+    menuService.getAllMenuItems().then(function (data) {
+        reply(data)
+            .code(200)
+            .header('Content-Type', 'application/json');
+    }).catch(function (error) {
+        // reply(boom.server_error(error.message))
         reply('error')
             .code(500)
             .type('application/json');
